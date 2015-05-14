8afa0310e650530d0dc675d6b3b56e76a77869ae
Steve Lemuel <wlemuel@hotmail.com>
2015-05-14 15:57:41 +0800

    Add screenshot

---
+++ app/src/main/java/tk/wlemuel/cotable/utils/onWebViewImageListener.java
@@ -0,0 +1,13 @@
+package tk.wlemuel.cotable.utils;
+
+/**
+ * onWebViewImageListener
+ *
+ * @author Steve Lemuel
+ * @version 0.1
+ * @desc onWebViewImageListener
+ * @created 2015/05/14
+ * @updated 2015/05/14
+ */
+public interface onWebViewImageListener {
+}
