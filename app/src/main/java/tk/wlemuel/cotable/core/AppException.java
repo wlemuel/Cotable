c0ed77cb98edebc5c8da0fe7fd817bd5023caef6
Steve Lemuel <wlemuel@hotmail.com>
2015-05-20 16:07:23 +0800

    Plan to add AppException class for handling the exceptions

---
+++ app/src/main/java/tk/wlemuel/cotable/core/AppException.java
@@ -0,0 +1,15 @@
+package tk.wlemuel.cotable.core;
+
+/**
+ * AppException
+ *
+ * @author Steve Lemuel
+ * @version 0.1
+ * @desc AppException
+ * @created 2015/05/20
+ * @updated 2015/05/20
+ */
+public class AppException {
+
+
+}
